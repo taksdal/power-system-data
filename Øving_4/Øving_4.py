9a70f1c86a25648b73ccb8905edd477a74121751
taksdal <ole.kristian.taksdal@gmail.com>
2026-09-10 16:04:26 +0200

    etter innlevering av Øving 4

--- Øving_4/Øving_4.py
+++ Øving_4/Øving_4.py
@@ -5,7 +5,7 @@ import matplotlib.dates as mdates
 from datetime import datetime, timezone
 import time
 
-df = pd.read_csv('/Users/olekristiantaksdal/repos/power-system-data/Øving 4/load_data.csv',
+df = pd.read_csv('/Users/olekristiantaksdal/repos/power-system-data/Øving_4/load_data.csv',
     decimal=",",
     parse_dates=['Time(Local)']
 )
@@ -13,13 +13,16 @@ df = pd.read_csv('/Users/olekristiantaksdal/repos/power-system-data/Øving 4/loa
 df.columns = df.columns.str.strip()
 df['Time(Local)'] = pd.to_datetime(
     df['Time(Local)'],
+    format="%d.%m.%Y %H:%M:%S %z",
     dayfirst=True,
     utc=True  
 )
-
 df = df.set_index('Time(Local)')
+
 #df = df.sort_index()
+
 df = df.tz_convert("Europe/Oslo")
+
 df["Netto"] = (df["Production"] - df["Consumption"])
 tot_prod = df["Production"].sum()
 tot_prod = tot_prod*10**(-3)
@@ -31,21 +34,13 @@ print("Indeks 0:")
 print(df.index[0])
 
 print("Info for kl 03:00:")
-print(df.loc["2026-01-01 03:00"])
+print(df.loc["2026-03-03 03:00"])
 
-dogn_profil = df.loc["2026-01-01 00:00":"2026-01-01 23:59"]
-# print(dogn_profil)
-# load_timer = dogn_profil["Production"].resample["h"].mean()
-# max = max(df["Production"])
-# print("Max produksjon =", max, "MW gjennom hele året")
-# min = min(df["Production"])
-# print("Min produksjon =", min, "MW gjennom hele året")
+#dogn_profil = df.loc["2026-01-01 00:00":"2026-01-01 23:59"]
+dogn_profil = df.loc["2026-03-03"]
+
 max_prod = max(dogn_profil["Production"])
 min_prod = min(dogn_profil["Production"])
-
-# print(type(dogn_profil["Production"]))
-# print(type(dogn_profil["Netto"]))
-# print(dogn_profil["Netto"])
 
 max_netto = round(max(dogn_profil["Netto"]), 2)
 min_netto = round(min(dogn_profil["Netto"]), 2)
@@ -145,6 +140,7 @@ plt.plot(df.index,
 #     y=["Production", "Consumption"],
 #     figsize = (10, 5)
 # )
+
 plt.title("Produksjon, forbruk og netto for hele året")
 plt.xlabel("Tid")
 
